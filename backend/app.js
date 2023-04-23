dde933f146a623ced5ff66a99d5f67836e2e6682
Madhur Saxena <saxenamadhur96@gmail.com>
2023-04-23 15:22:25 +0530

    MS | Frontend Deployment -3

--- backend/app.js
+++ backend/app.js
@@ -31,15 +31,10 @@ app.use(cors({
 
 
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-    );
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
-
+    res.setHeader('Referrer-Policy', 'no-referrer');
     next();
-}); //cors error
+});
+
 
 // const multer = require('multer')
 // const storage  = multer.diskStorage({
