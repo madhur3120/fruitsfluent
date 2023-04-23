cbd473e072ca9168937907a3fd29b1cdd10cfaac
Madhur Saxena <saxenamadhur96@gmail.com>
2023-04-23 11:41:30 +0530

    MS | Deployment of backend

--- backend/models/ordermodel.js
+++ backend/models/ordermodel.js
@@ -1,18 +1,18 @@
 const mongoose = require("mongoose")
 
 const orderschema = new mongoose.Schema({
-    username : {
-        type : String
+    username: {
+        type: String
     },
-    email : {
-        type : String
+    email: {
+        type: String
     },
-    products : {
-        type : String
+    products: {
+        type: String
     },
-    ordertotal : {
-        type : String        
+    ordertotal: {
+        type: String
     }
 })
 
-module.exports = mongoose.model("orders",orderschema)
+module.exports = mongoose.model("orders", orderschema)
